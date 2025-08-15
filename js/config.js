b6e13003811d32959d786f6d71788b71472195f3
Nvetto <nvettovalli@gmail.com>
2025-08-15 13:31:12 -0300

    backend en render

--- js/config.js
+++ js/config.js
@@ -4,4 +4,4 @@ const esEntornoLocal = window.location.hostname === '127.0.0.1';
 // Definimos la URL del backend según el entorno
 const API_BASE_URL = esEntornoLocal 
     ? 'http://127.0.0.1:5000' // URL para desarrollo local
-    : 'URL_DE_TU_BACKEND_EN_RENDER'; // ¡Aquí irá la URL de Render más adelante!
+    : 'https://magknives-backend.onrender.com'; 
